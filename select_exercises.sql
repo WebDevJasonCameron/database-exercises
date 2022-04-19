e31df36a03c69df53e74d6b80429d8b62eaa4623
Jason Cameron <dev.jasoncameron@gmail.com>
2022-04-18 20:21:33 -0500

    comp sql ex part 3

--- select_exercises.sql
+++ select_exercises.sql
@@ -5,3 +5,21 @@ SELECT name FROM albums
 # The year Sgt. Pepper's Lonely Hearts Club Band was released
 SELECT release_date FROM albums
     WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+# The genre for Nevermind
+SELECT * FROM albums
+    WHERE genre = 'Nevermind';
+
+# Which albums were released in the 1990s
+SELECT * FROM albums
+    WHERE release_date > 1989 AND albums.release_date < 2000;
+
+# Which albums had less than 20 million certified sales
+SELECT * FROM albums
+    WHERE sales > 20;
+
+# All the albums with a genre of "Rock". Why do these query results not
+# include albums with a genre of "Hard rock" or "Progressive rock"?
+SELECT * FROM albums
+    WHERE genre LIKE '%rock%'
+    OR genre LIKE '%Rock%';
