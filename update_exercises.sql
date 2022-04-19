2d306b5bc4a83ea0d126776145c11e67b5c1cf4f
Jason Cameron <dev.jasoncameron@gmail.com>
2022-04-18 20:50:41 -0500

    comp sql ex

---
+++ update_exercises.sql
@@ -0,0 +1,45 @@
+-- Write SELECT statements to output each of the following with a caption:
+# All albums in your table.
+SELECT artist AS 'Rock Group',
+    name AS 'Album Title',
+    release_date AS 'Date Released',
+    sales AS 'Total Sales',
+    genre
+    FROM albums;
+
+# After each SELECT add an UPDATE statement to:
+# Make all the albums 10 times more popular (sales * 10)
+
+# All albums released before 1980
+SELECT artist AS 'Rock Group',
+       name AS 'Album Title',
+       release_date AS 'Date Released',
+       sales AS 'Total Sales',
+       genre
+    FROM albums
+    WHERE release_date < 1980;
+
+UPDATE albums
+    SET sales = sales * 10
+    WHERE release_date < 1980;
+
+#  All albums by Michael Jackson
+SELECT artist AS 'Rock Group',
+       name AS 'Album Title',
+       release_date AS 'Date Released',
+       sales AS 'Total Sales',
+       genre
+    FROM albums
+    WHERE artist = 'MICHAEL JACKSON';
+
+UPDATE albums
+    SET sales = sales * 10
+    WHERE artist = 'MICHAEL JACKSON';
+
+UPDATE albums
+    SET release_date = release_date - 100
+    WHERE release_date < 1980;
+
+UPDATE albums
+    SET name = 'Peter Jackson'
+    WHERE name = 'Michael Jackson';
